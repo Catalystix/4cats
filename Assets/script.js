7e9aadeeb08b8c4170990b8224c843cbe6e67b8a
Catalystix <dcode.green@gmail.com>
2022-11-18 21:30:35 -0500

    testing

--- Assets/script.js
+++ Assets/script.js
@@ -4,3 +4,10 @@ var aboutme=document.querySelector(".bio")
 aboutme.textContent="Bring me treats!"
 
 })
+
+var button1=document.getElementById("button-2")
+button. addEventListener("click",function(){
+var aboutme=document.querySelector(".bio")
+aboutme.textContent="Bring me treats!"
+
+})
